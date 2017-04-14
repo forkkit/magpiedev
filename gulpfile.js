1e2f87c25fb8d633ac01ab9ab6c119492f3ec215
Steve Lacy <me@slacy.me>
2017-04-13 22:56:31 -0700

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,131 @@
+const fs = require('fs')
+const http = require('http')
+const gulp = require('gulp')
+const es = require('ecstatic')
+const pug = require('pug')
+const fecha = require('fecha')
+const marked = require('marked')
+const highlight = require('highlight.js')
+const through = require('through2')
+const sass = require('gulp-sass')
+const gpug = require('gulp-pug')
+const reload = require('gulp-livereload')
+const rename = require('gulp-rename')
+const cssmin = require('gulp-clean-css')
+const prefixer = require('gulp-autoprefixer')
+const autowatch = require('gulp-autowatch')
+
+const siteConfig = require('./src/config.json')
+
+pug.filters.markdown = (string) => {
+  if (string.substr(-3) === '.md') {
+    string = fs.readFileSync(string, 'utf8')
+  }
+  const renderer = new marked.Renderer()
+  renderer.code = (text, lang) => {
+    text = highlight.highlightAuto(text).value
+    return '<div class="markdown-code"><pre><code class="lang-' + lang + '">' + text + '</code></pre></div>'
+  }
+  marked.setOptions({
+    renderer: renderer,
+    gfm: true,
+    tables: true,
+    breaks: false,
+    pedantic: false,
+    sanitize: false,
+    smartLists: true,
+    smartypants: false,
+    // highlight: (code) => highlight.highlightAuto(code).value
+  })
+  return marked(string)
+}
+
+const config = {
+  port: 5000,
+  paths: {
+    js: './src/**/*.js',
+    pug: [ './src/**/*.pug', '!./src/**/_*.pug', './src/**/*.md' ],
+    sass: './src/**/*.sass',
+    images: ['./src/**/*.jpg', './src/**/*.svg', './src/**/*.png'],
+    pages: ['./src/pages/**/index.json']
+  },
+  src: {
+    js: './src/**.*.js',
+    pug: [ './src/**/*.pug', '!./src/**/_*.pug' ],
+    sass: './src/index.sass'
+  },
+  dist: './dist'
+}
+
+gulp.task('js', (cb) => {
+  cb()
+})
+
+gulp.task('pages', () => {
+  const pages = []
+  return gulp.src(config.paths.pages)
+    .pipe(((opts) => {
+      return through.obj((file, inc, cb) => {
+        const page = JSON.parse(String(file.contents))
+        page.path = file.dirname.substr(file.dirname.lastIndexOf('/') + 1)
+        page.image = '/pages/' + page.path + '/' + page.image
+        const date = new Date(JSON.parse(file.contents).date)
+        page.date = fecha.format(date, 'MMM Do, YYYY')
+        pages.push(page)
+        cb(null, file)
+      })
+      .on('end', () => {
+        siteConfig.pages = pages
+        const prettyJson = JSON.stringify(siteConfig, null, 2)
+        fs.writeFileSync('./src/config.json', prettyJson)
+      })
+    })())
+})
+
+gulp.task('pug', gulp.series(['pages'], () => {
+  return gulp.src(config.src.pug)
+    .pipe(gpug({
+      pug,
+      locals: siteConfig
+    }))
+    .pipe(rename(function (path) {
+      if (path.basename !== 'index') {
+        path.dirname  = path.dirname + '/' + path.basename
+        path.basename = 'index'
+      }
+    }))
+    .pipe(gulp.dest(config.dist))
+    .pipe(reload())
+}))
+gulp.task('sass', () => {
+  return gulp.src(config.src.sass)
+    .pipe(sass())
+    .pipe(prefixer({
+      browsers: ['last 4 versions'],
+      cascade: false
+    }))
+    .pipe(cssmin())
+    .pipe(gulp.dest(config.dist))
+    .pipe(reload())
+})
+
+gulp.task('images', () => {
+  return gulp.src(config.paths.images)
+    .pipe(gulp.dest(config.dist))
+    .pipe(reload())
+})
+
+gulp.task('server', (cb) => {
+  const server = http.createServer(es({ root: config.dist }))
+  server.listen(config.port, cb)
+  console.log('Starting server on port', config.port)
+})
+
+gulp.task('watch', (cb) => {
+  reload.listen()
+  autowatch(gulp, config.paths)
+  cb()
+})
+
+gulp.task('build', gulp.parallel(Object.keys(config.paths)))
+gulp.task('default', gulp.series(['server', 'watch', 'build']))
---
+++ src/pages/create-an-ios-inspired-toggle-switch-in-react/index.md
@@ -0,0 +1,99 @@
+
+Hey! Here is a simple React toggle switch component that can be used for any mobile inspired website, or hybrid app.
+
+<div class="spacer h25"></div>
+
+## The React
+
+This Component only requires one state key for the toggle, aptly named `toggle`.
+
+First, add the imports and the DOM render node:
+
+```js
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// Component will go here.
+
+ReactDOM.render(<Toggle />, document.getElementById('root'))
+
+```
+
+Now, create a class extending the React Component and insert it where our prior comment was:
+
+
+```js
+
+class Toggle extends React.Component {
+  state = {
+    toggle: false
+  }
+
+  toggle () {
+    this.setState({ toggle: !this.state.toggle })
+  }
+}
+```
+
+To continue, we will add the JSX for the actual DOM elements in the render function:
+The className will add the `toggle` class when the toggle is active.
+
+```js
+render () {
+  const className = `toggle-component ${ this.state.toggle ? ' active' : ''}`
+  return (
+    <div
+      className={className}
+      onClick={() => this.toggle()}>
+      <div className='toggle-button' />
+    </div>
+  )
+}
+```
+
+## CSS
+
+```css
+.wrapper {
+  padding: 50px;
+}
+
+.toggle-component {
+  font-size: 2rem;
+  color: #171717;
+  width: 50px;
+  height: 20px;
+  background: #777777;
+  border: 1px solid #555;
+  position: relative;
+  border-radius: 10px;
+  cursor: pointer;
+  transition: border-color background-color 0.3s;
+}
+.toggle-component.active {
+  background: #66CC99;
+  border-color: #dfdfdf;
+}
+.toggle-component.active .toggle-button {
+  right: -2px;
+}
+.toggle-component .toggle-button {
+  position: absolute;
+  top: -3px;
+  right: 50%;
+  height: 26px;
+  width: 26px;
+  background: #dfdfdf;
+  box-shadow: 0 0 5px #555;
+  border-radius: 50%;
+  transition: right 0.3s;
+}
+```
+
+
+## JSFiddle Demo
+All together now:
+
+<iframe width="100%" height="300" src="//jsfiddle.net/stevelacy/tnp9m9zc/9/embedded/" allowfullscreen="allowfullscreen" frameborder="0"></iframe>
+
+
